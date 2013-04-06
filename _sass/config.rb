ba16db855edd207dfb9b42e9282d1f40666ef4bb
Sean Hendrickson <seandrickson@gmail.com>
2013-04-06 15:07:51 -0700

    revisions to sass

--- _sass/config.rb
+++ _sass/config.rb
@@ -2,11 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+css_dir = "../css"
+sass_dir = "./"
+images_dir = "../img"
+javascripts_dir = "../js"
+fonts_dir = "../fonts"
 
 output_style = :compressed
 environment = :production
