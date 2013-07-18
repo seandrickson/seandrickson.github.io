9f28acb384c7edcc66e18d95b7d6c0ede2001dbb
Sean <seandrickson@gmail.com>
2013-07-18 09:51:05 -0700

    removed neat dependancy

--- js/behance.js
+++ js/behance.js
@@ -5,29 +5,31 @@ FlyJSONP.get({
     url: 'http://www.behance.net/v2/users/seandrickson/projects',
     parameters: { api_key: 'Am3dYZpehliXhXrGIB1Lq831ivWfmBQF' },
     success: function(data) {
-        if (data.http_code === 200) {
-            var d = document, k,
-                bpro = data.projects,
-                load = d.getElementById('loading'),
-                port = d.getElementById('portfolio');
-            load.parentNode.removeChild(load);
+        window.onload = (function(data) {
+            if (data.http_code === 200) {
+                var d = document, k,
+                    bpro = data.projects,
+                    load = d.getElementById('loading'),
+                    port = d.getElementById('portfolio');
+                load.parentNode.removeChild(load);
 
-            for (k in bpro) {
-                var div = d.createElement('div'),
-                    a = d.createElement('a'),
-                    img = d.createElement('img');
+                for (k in bpro) {
+                    var div = d.createElement('div'),
+                        a = d.createElement('a'),
+                        img = d.createElement('img');
 
-                img.setAttribute('src', bpro[k].covers[404]);
-                img.setAttribute('alt', bpro[k].name);
-                a.appendChild(img);
+                    img.setAttribute('src', bpro[k].covers[404]);
+                    img.setAttribute('alt', bpro[k].name);
+                    a.appendChild(img);
 
-                a.setAttribute('href', bpro[k].url);
-                div.appendChild(a);
+                    a.setAttribute('href', bpro[k].url);
+                    div.appendChild(a);
 
-                div.setAttribute('class', 'project one-fourth');
-                port.appendChild(div);
-            }
-        } else { this.error(data); }
+                    div.setAttribute('class', 'project one-fourth');
+                    port.appendChild(div);
+                }
+            } else { this.error(data); }
+        }(data));
     },
     error: function(error) {
         console.log(error);
